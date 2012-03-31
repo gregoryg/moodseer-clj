ae5d95c4754e12ff497460622514d8fc32b827e2
gregoryg <ggrubbs@grubbs-g.us.syncsort.com>
2012-03-31 11:52:15 -0700

    Added dependencies for jdbc and clj-json

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
 (defproject moodseer-clj "0.0.1"
   :description "Write a browser front end to the Moodseer streaming audio server"
   :dependencies [[org.clojure/clojure "1.3.0"]
-		 [clj-json "0.3.1"]
+                 [org.clojure/java.jdbc "0.1.1"]
+		 [mysql/mysql-connector-java "5.1.17"]
+		 [clj-json "0.5.0"]
 		 [noir "1.3.0-beta2"]
 		 [enlive "1.0.0-SNAPSHOT"]
 		 ]
   :plugins      [[lein-swank "1.4.3"]]
-  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
+                     [org.clojure/java.jdbc "0.1.1"]]
   )
