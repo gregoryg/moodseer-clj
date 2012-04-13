8ccde643f779250801725d3e5cba402fe7ab01f9
gregoryg <ggrubbs@grubbs-g.us.syncsort.com>
2012-04-13 17:01:15 -0600

    Refactored for reusable functions.  Set timeoutinterval for auto refreshes of song data and upcoming songs

--- resources/public/js/standard.js
+++ resources/public/js/standard.js
@@ -6,10 +6,10 @@ var moodseer_server_url = moodseer_host + "/lib/ajaxresults.php";
 var moodseer_stream_url = moodseer_host + ":" + moodseer_port + "/stream" + moodseer_station;
 
 function imagerollover() {
-    $("img[data-over]", "#playbuttons").mouseover(function() {
+    $("img[data-over]", "#playbuttons,#controlbuttons").mouseover(function() {
 	$(this).attr("src", $(this).attr("data-over"));
     });
-    $("img[data-over]", "#playbuttons").mouseout(function() {
+    $("img[data-over]", "#playbuttons,#controlbuttons").mouseout(function() {
 	$(this).attr("src", $(this).attr("data-out"));
     });
 } // end function imagerollover
@@ -32,7 +32,41 @@ function populateSongData(data, stat) {
     $("div#nowplaying .album-image img").attr("src", "http://172.16.17.17/" + data.nowplaying.coverart_url).attr("alt", "Album cover art");
 }
 
+function populateUpcoming(data, stat) {
+    var items = [];
+    $.each(data.upcoming, function(i, item) {
+	items.push("<li>" + item.song + " - " + item.artist + "</li>");
+    });
+    $("#experimental #upcoming ol.songlist").empty().append(items.join('\n'));
+}
+
+function updateSongData() {
+    $.getJSON(moodseer_server_url,
+	      "cmd=nowplaying&stationNum=1&station=1&window=1",
+	      populateSongData);
+}
+
+function updateUpcoming() {
+    $.getJSON(moodseer_server_url,
+	      "cmd=upcoming&stationNum=1&station=1&window=10",
+	      populateUpcoming);
+}
+
+function toggleMute() {
+    sendMoodseerCommand("toggle_mute", function(data, stat) {
+	if (data.volume.muted == "true") {
+	    $("#controlbuttons #mute img.mute-off").hide();
+	    $("#controlbuttons #mute img.mute-on").show();
+	} else {
+	    $("#controlbuttons #mute img.mute-off").show();
+	    $("#controlbuttons #mute img.mute-on").hide();
+	}
+    });
+}
+
 $(document).ready(function() {
+    $("#controlbuttons #mute img.mute-on").hide();
+    $("#playbuttons #play img.paused").hide();
     imagerollover();
     $("div#playbuttons #play").click(function() {
 	sendMoodseerCommand("play", function(data, stat) {
@@ -54,6 +88,11 @@ $(document).ready(function() {
 
     });
 
+
+    $("div#controlbuttons #mute").click(function() {
+	toggleMute();
+    });
+
     $("div#playbuttons #back").click(function() {
 	sendMoodseerCommand("prev", function(data, stat) {
 	    populateSongData(data, stat);
@@ -61,19 +100,10 @@ $(document).ready(function() {
 
     });
 
-    $("div#experimental #getupcoming").click(function() {
-	// get a list of upcoming songs on stream/station 1
-	$.getJSON(moodseer_server_url,
-		  "cmd=upcoming&stationNum=1&station=1&window=10",
-		  function(data, stat) {
-		      var items = [];
-		      $.each(data.upcoming, function(i, item) {
-		      	  items.push("<li>" + item.song + "</li>")
-		      });
-		      
-		      $("#experimental #upcoming ol.songlist").empty().append(items.join('\n'));
-		  });
-    });
+    updateSongData();
+    updateUpcoming();
+    setInterval(function() { updateSongData(); updateUpcoming();
+			   }, 20000);
 
 });
-    
+
