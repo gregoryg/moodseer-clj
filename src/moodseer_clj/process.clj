2a2e53be839f7ae151991136bfc62e86e8db532b
gregoryg <gregory@dynapse.com>
2012-04-15 11:38:28 -0600

    Added support for mpc/mpd - Control is much better than with mplayer in slave mode.
    defn get-player-nowplaying - get metadata from mpc, return map as before
    
    defn get-player-progress - return map of player progress, including
         :state "playing" or "paused"
         :playlist-pos current playing song position in active playlist
         :playlist-count total number of songs in active playlist
         :time-played time played so far as min:sec
         :song-length total length of song as min:sec
         :percent-played percentage played

--- src/moodseer_clj/process.clj
+++ src/moodseer_clj/process.clj
@@ -5,6 +5,16 @@
 ;; (sh "bash" "-c" "mplayer -playlist /tmp/gort.m3u -shuffle -quiet -slave -input file=/tmp/MYCMD  1>/tmp/mplayer-out.log 2>&1 & \n echo $!")
 ;; (sh "bash" "-c" "echo 'get_meta_artist\nget_meta_title\nget_meta_album\nget_meta_genre\nget_meta_year\nget_meta_track\nget_meta_comment' > /tmp/MYCMD")
 ;; (sh "bash" "-c" "echo 'get_percent_pos\nget_time_pos\nget_time_length' > /tmp/MYCMD")
+;; mpc current --format "file=%file%\nartist=%artist%\nalbum=%album%\nsong=%title%\ntrack-num=%track%\ntime=%time%\n"
+
+;; file=Various-Brazil_The_Music_of/02.Dominguinhos_Convidados_(w_Marciel_Melo)-A_Volta_Da_Asa_Branca.mp3
+;; artist=Dominguinhos & Convidados (w/ Marciel Melo)
+;; album=Brazil, The Music of
+;; song=A Volta Da Asa Branca
+;; track-num=02
+;; time=3:49
+;; ORBIT_SOCKETDIR=/tmp/orbit-gregj
+
 
 (def fifo-path "/tmp/MYCMD")
 
@@ -34,26 +44,33 @@
       (sh "mkfifo" fifo-path))))
 
 
-(defn start-player [& playlist]
-  (do
-    (mkfifo)
-    (Thread/sleep 200)
-    (println (str player-cmd " " (clojure.string/join " " playlist) " 1>/tmp/mplayer-out.log 2>&1 & \n echo $!"))
-    (sh "bash" "-c" (str player-cmd " " (first playlist) " 1>/tmp/mplayer-out.log 2>&1 & \n echo $!"))))
+;; (defn start-player [& playlist]
+;;   (do
+;;     (mkfifo)
+;;     (Thread/sleep 200)
+;;     (println (str player-cmd " " (clojure.string/join " " playlist) " 1>/tmp/mplayer-out.log 2>&1 & \n echo $!"))
+;;     (sh "bash" "-c" (str player-cmd " " (first playlist) " 1>/tmp/mplayer-out.log 2>&1 & \n echo $!"))))
+
+;; (defn pause-player []
+;;   (sh "bash" "-c" (str "echo 'pause' > " fifo-path) ))
+(defn play-player []
+  (sh "mpc" "play"))
 
 (defn pause-player []
-  (sh "bash" "-c" (str "echo 'pause' > " fifo-path) ))
+  (sh "mpc" "toggle"))
 
-(defn step-player
-  ([] (step-player 1))
-  ([step]
-     (sh "bash" "-c" (str "echo 'pt_step " step "' > " fifo-path))))
+
+
+;; (defn step-player
+;;   ([] (step-player 1))
+;;   ([step]
+;;      (sh "bash" "-c" (str "echo 'pt_step " step "' > " fifo-path))))
 
 (defn next-player []
-  (step-player 1))
+  (sh "mpc" "next"))
 
 (defn prev-player []
-  (step-player -1))
+  (sh "mpc" "prev"))
 
 (defn quit-player []
   (sh "bash" "-c" (str "echo 'quit' > " fifo-path) ))
@@ -65,9 +82,43 @@
         ]
     y))
 
-(defn get-player-progress []
+(defn MPLAYER-get-player-progress []
   (let [x (sh "bash" "-c" "echo 'get_percent_pos\nget_time_pos\nget_time_length' > /tmp/MYCMD")
         _ (Thread/sleep 200)
         y (:out (sh "bash" "-c" "tail -3 /tmp/mplayer-out.log | egrep '^ANS_' | sed \"s,',,g\""))
         ]
     y))
+
+(defn player-nowplaying []
+  (->> 
+   (sh "mpc" "current" "--format" "file=%file%\nartist=%artist%\nalbum=%album%\nsong=%title%\ntrack-num=%track%\ntime=%time%\n")
+   :out
+   mplayer-pairs->map
+   ))
+
+(defn get-player-progress []
+  (let [x (:out (sh "mpc" "--format" "file=%file%\ntime=%time%\n"))
+        ;; _ (println x)
+        ; pick out components: playlist_pos playlist_count time_played time_length percent_played
+        progress-fields [:state :playlist-pos :playlist-count :time-played :song-length :percent-played]
+        y  (into {} (map #(hash-map %1 %2)
+                         progress-fields
+                         (rest (first (re-seq #"\n?\[(playing|paused)\]\s+#([0-9]+)/([0-9]+)\s+([0-9:]+)/([0-9:]+)\s+\(([0-9]+)%\)" x)))
+                         ))
+        z (into y (map #(hash-map (keyword (nth % 1)) (nth % 2))
+                       (re-seq #"(volume|repeat|random|single|consume):\s+([^ \n]+)" x)))
+        ]
+    z))
+
+(defn get-player-upcoming
+  "Get pre-formatted upcoming songs"
+  ([] (get-player-upcoming 10))
+  ([n] (let [x (:out (sh "bash" "-c" (str "mpc playlist | head -" n)))]
+         (str/split x #"\n"))))
+
+(defn get-player-upcoming-paths
+  "Get paths to files in upcoming list"
+  ([] (get-player-upcoming-paths 10))
+  ([n] (let [x (:out (sh "bash" "-c" (str "mpc listall | head -" n)))]
+         x)))
+  
