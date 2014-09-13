6c986e4e7a283897d52d4913d08be77fcda58347
gregoryg <gregory@dynapse.com>
2014-09-13 13:29:22 -0600

    Added first test using ring-mock and clojure.test

--- test/moodseer_clj/core_test.clj
+++ test/moodseer_clj/core_test.clj
@@ -1,7 +1,16 @@
 (ns moodseer-clj.core-test
   (:require [clojure.test :refer :all]
+            [ring.mock.request :refer :all]
             [moodseer-clj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; (deftest a-test
+;;   (testing "FIXME, I fail."
+;;     (is (= 0 1))))
+
+(deftest another-test
+  (testing "Random API test"
+    (is 
+     (= "Do something with req"
+        (-> (request :get "/" )
+            app
+            :body)))))
